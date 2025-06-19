0cb0b37a68fb0b0f9d8d6e52d6a86b0787db4ee8
SaraivaZAQ! <leandro-0806@hotmail.com>
2025-06-18 21:45:19 -0300

    first commit

---
+++ src/main/java/dev/saraiva/AirTravelManagement/AirTravelManagementApplication.java
@@ -0,0 +1,13 @@
+package dev.saraiva.AirTravelManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirTravelManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirTravelManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/saraiva/AirTravelManagement/AirTravelManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.saraiva.AirTravelManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirTravelManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
